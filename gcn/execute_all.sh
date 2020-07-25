3796de6dffe981a7c7c91d95bf9a2ba6a51f7de7
Ronald <ronald.drp11@gmail.com>
2020-07-25 06:15:31 -0300

    added execute all tests bash script

---
+++ gcn/execute_all.sh
@@ -0,0 +1,7 @@
+python train.py --dataset amlsim_sample --model gcn > output/amlsim_sample_gcn.txt
+python train.py --dataset amlsim_sample --model gcn_cheby > output/amlsim_sample_gcn_cheby.txt
+python train.py --dataset amlsim_sample --model dense > output/amlsim_sample_dense.txt
+
+python train.py --dataset amlsim --model gcn > output/amlsim_gcn.txt
+python train.py --dataset amlsim --model gcn_cheby > output/amlsim_gcn_cheby.txt
+python train.py --dataset amlsim --model dense > output/amlsim_dense.txt
