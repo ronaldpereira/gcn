e78eea22386020878655bd5cefd89db6a32da0bd
Ronald <ronald.drp11@gmail.com>
2020-12-07 14:53:08 -0300

    uncommented all experiments

--- gcn/execute_all.sh
+++ gcn/execute_all.sh
@@ -1,19 +1,19 @@
-# python amlsim_process.py data/transactions_sample.csv data/ --filename amlsim_sample
-# python train.py --dataset amlsim_sample --model gcn > output/amlsim_sample_gcn.txt
-# python train.py --dataset amlsim_sample --model gcn_cheby > output/amlsim_sample_gcn_cheby.txt
-# python train.py --dataset amlsim_sample --model dense > output/amlsim_sample_dense.txt
+python amlsim_process.py data/transactions_sample.csv data/ --filename amlsim_sample
+python train.py --dataset amlsim_sample --model gcn > output/amlsim_sample_gcn.txt
+python train.py --dataset amlsim_sample --model gcn_cheby > output/amlsim_sample_gcn_cheby.txt
+python train.py --dataset amlsim_sample --model dense > output/amlsim_sample_dense.txt
 
-# python amlsim_process.py data/transactions.csv data/ --filename amlsim
-# python train.py --dataset amlsim --model gcn > output/amlsim_gcn.txt
-# python train.py --dataset amlsim --model gcn_cheby > output/amlsim_gcn_cheby.txt
-# python train.py --dataset amlsim --model dense > output/amlsim_dense.txt
+python amlsim_process.py data/transactions.csv data/ --filename amlsim
+python train.py --dataset amlsim --model gcn > output/amlsim_gcn.txt
+python train.py --dataset amlsim --model gcn_cheby > output/amlsim_gcn_cheby.txt
+python train.py --dataset amlsim --model dense > output/amlsim_dense.txt
 
-# python amlsim_process.py data/transactions_10k.csv data/ --filename amlsim_10k
-# python train.py --dataset amlsim_10k --model gcn > output/amlsim_10k_gcn.txt
-# python train.py --dataset amlsim_10k --model gcn_cheby > output/amlsim_10k_gcn_cheby.txt
-# python train.py --dataset amlsim_10k --model dense > output/amlsim_10k_dense.txt
+python amlsim_process.py data/transactions_10k.csv data/ --filename amlsim_10k
+python train.py --dataset amlsim_10k --model gcn > output/amlsim_10k_gcn.txt
+python train.py --dataset amlsim_10k --model gcn_cheby > output/amlsim_10k_gcn_cheby.txt
+python train.py --dataset amlsim_10k --model dense > output/amlsim_10k_dense.txt
 
-# python amlsim_process.py data/transactions_oversample.csv data/ --filename amlsim_oversample
+python amlsim_process.py data/transactions_oversample.csv data/ --filename amlsim_oversample
 python train.py --dataset amlsim_oversample --model gcn > output/amlsim_oversample_gcn.txt
 python train.py --dataset amlsim_oversample --model gcn_cheby > output/amlsim_oversample_gcn_cheby.txt
 python train.py --dataset amlsim_oversample --model dense > output/amlsim_oversample_dense.txt
